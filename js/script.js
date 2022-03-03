e9d93b350ce1e15b3f1b3039b8432fd7382a202f
Kireeva <kireeva.en@defo.ru>
2022-03-03 15:14:50 +0300

    пытаюсь залить файлы Vino

--- js/script.js
+++ js/script.js
@@ -1,7 +1,91 @@
-new Swiper('.swiper-container');  
+"use strict"
 
+// -- Определяем на каком устройстве открыта страница
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
+
+// - если открыта на мобилке (есть тачскрин)
+if (isMobile.any()) {
+    document.body.classList.add('_touch');
+
+    let menuArrows = document.querySelectorAll('.menu__arrow');
+
+    if (menuArrows.length > 0) {
+        for (let index = 0; index < menuArrows.length; index++){
+            const menuArrow = menuArrows[index];
+            menuArrow.addEventListener('click', function(e) {
+                menuArrow.parentElement.classList.toggle('_active');
+            });
+        }
+    }
+}else{
+    document.body.classList.add('_pc');
+};
+
+
+// -- меню бургер
+// const iconMenu = document.querySelector('.menu__icon');
+// const menuBody = document.querySelector('.menu__body');
+
+// if(iconMenu){
+//     iconMenu.addEventListener('click', function(e){
+//         document.body.classList.toggle('_lock');
+//         iconMenu.classList.toggle('_active');
+//         menuBody.classList.toggle('_active');
+//     });
+// }
+
+
+// Переключение вкладок меню
+$(".menu ul li").click(function(e) {
+    e.preventDefault();
+    $(".menu ul li").removeClass('active');
+    $(this).addClass('active');
+  })
+
+
+  // Мобильный футер
+  $(".footer__item").click(function(e) {
+    e.preventDefault();
+    $(this).toggleClass('active');
+  })
  
 
+// Счетчик кол-ва товаров внутри карточки
+  $(document).ready(function() {
+    $('.counter__minus').click(function () {
+        var $input = $(this).parent().find('input');
+        var count = parseInt($input.val()) - 1;
+        count = count < 1 ? 1 : count;
+        $input.val(count);
+        $input.change();
+        return false;
+    });
+    $('.counter__plus').click(function () {
+        var $input = $(this).parent().find('input');
+        $input.val(parseInt($input.val()) + 1);
+        $input.change();
+        return false;
+    });
+});
 
 
- 
